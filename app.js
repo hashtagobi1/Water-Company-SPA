e7fc3b086ac8630f2a91074ae477dd194dc82082
Obi <anokwuruobi@gmail.com>
2021-04-19 17:00:57 +0100

    Ready to upload!

--- app.js
+++ app.js
@@ -1,4 +1,7 @@
 "use strict";
+
+// Turns on Nav bar at a cetain scroll point on page
+// .add + .remove to create + remove classes
 window.onload = function () {
     window.addEventListener('scroll', function (e) {
         if(window.pageYOffset > 768){
@@ -11,3 +14,8 @@ window.onload = function () {
 }; 
 //know shit works
 // https://stackoverflow.com/questions/1626107/text-in-html-field-to-disappear-when-clicked
+
+
+// Calling Class
+
+const swup = new Swup();
